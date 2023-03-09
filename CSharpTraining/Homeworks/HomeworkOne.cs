a69b9f5bba94a304eb8cb0cd3973772e4a5e5bb9
ciprian.iluta <cainari.iluta@mail.ru>
2023-03-09 16:39:55 +0200

    Add Program.cs and main method

--- CSharpTraining/Homeworks/HomeworkOne.cs
+++ CSharpTraining/Homeworks/HomeworkOne.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("If you see this, then it means that my repository was published.");
+﻿using System;
+
+class HomeworkOne {
+    public static void Run()
+    {
+        Console.WriteLine("If you see this, then it means that my repository was published.");
+    }
+}
---
+++ CSharpTraining/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CSharpTraining {
+    class Program {
+        static void Main(string[] args)
+        {
+            HomeworkOne.Run();
+        }
+    }
+}
