a6ac43e29fd318724a52f1913a58b2e2e7c56b40
ciprian.iluta <cainari.iluta@mail.ru>
2023-03-17 13:17:14 +0200

    Resolve sonnar issues

--- Homework1/HomeworkOne.cs
+++ Homework1/HomeworkOne.cs
@@ -1,8 +1,8 @@
-﻿using System;
-
-class HomeworkOne {
-    public static void Run()
-    {
-        Console.WriteLine("If you see this, then it means that my repository was published.");
+﻿namespace Homework1 {
+    public static class HomeworkOne {
+        public static void Run()
+        {
+            Console.WriteLine("If you see this, then it means that my repository was published.");
+        }
     }
 }
--- Homework2/CommonUtils/NumbersEnum.cs
+++
@@ -1,5 +0,0 @@
-﻿public enum NumbersEnum {
-    One = 1,
-    Ten = 10,
-    FortySeven = 47
-}
