a7c5581f1c8bb67d5f649b2ebd9b046ecb5d9159
ciprian.iluta <cainari.iluta@mail.ru>
2023-03-23 16:51:14 +0200

    Add homework3

---
+++ Homework3/Program.cs
@@ -0,0 +1,12 @@
+﻿using Homework3.Gearboxes;
+
+namespace Homework3 {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            ManualGearbox manualGearbox = new ManualGearbox();
+            Car manualCar = new Car("Toyota", "Prius", 1996, 4, manualGearbox);
+            Console.WriteLine(manualCar.ToString);
+        }
+    }
+}
---
+++ Homework3/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework3 {
+    public class Vehicle {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public virtual void Start()
+        {
+            Console.WriteLine($"Starting...");
+        }
+
+        public virtual void Stop()
+        {
+            Console.WriteLine($"Stopping...");
+        }
+
+    }
+}
