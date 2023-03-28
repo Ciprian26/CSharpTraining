67efb06922b252e021fa58777da40f4f66ac4e5b
ciprian.iluta <cainari.iluta@mail.ru>
2023-03-28 17:24:28 +0300

    Add homework 4 implementation

---
+++ Homework4/Program.cs
@@ -0,0 +1,26 @@
+﻿using static UtilsLibrary.ListUtils;
+using static UtilsLibrary.InputUtils;
+using static UtilsLibrary.ConsoleUtils;
+
+using Homework4.Implementations.ArrayType;
+using Homework4.Implementations.ListType;
+
+namespace Homework4
+{
+    public static class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementation: \n1. List\n2. Array\nEnter desired implementation:");
+
+            switch(int.Parse(Console.ReadLine()))
+            {
+                case 1:
+                    ProgramList.Run();
+                    break;
+                case 2:
+                    ProgramArray.Run();
+                    break;
+            }
+        }
+    }
+}
